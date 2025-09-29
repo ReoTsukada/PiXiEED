a8770ed57931071460164b74158fc19639139060
ARTA <rgaydm03@gmail.com>
2025-09-29 09:36:56 +0900

    Create manifest.js

---
+++ portfolio/dots/manifest.js
@@ -0,0 +1,14 @@
+window.DOT_GALLERY_MANIFEST = [
+  {
+    "file": "ARTA_HinyariToNyanbiri.gif",
+    "label": "ARTA HinyariToNyanbiri",
+    "date": "2025-09-29",
+    "alt": "ARTA HinyariToNyanbiri"
+  },
+  {
+    "file": "ARTA_TabunnItumonoShibuya.gif",
+    "label": "ARTA TabunnItumonoShibuya",
+    "date": "2025-09-29",
+    "alt": "ARTA TabunnItumonoShibuya"
+  }
+];
