e0568bbe399be88ab9dbeae0fd9e2b68d06f1f0e
ARTA <rgaydm03@gmail.com>
2026-02-02 10:13:27 +0900

    Update supabase.js

--- contest/assets/js/supabase.js
+++ contest/assets/js/supabase.js
@@ -2,5 +2,5 @@
 export const SUPABASE_URL = 'https://kyyiuakrqomzlikfaire.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_gnc61sD2hZvGHhEW8bQMoA_lrL07SN4';
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.46.1/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.46.1?bundle';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
