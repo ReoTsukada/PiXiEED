2b3129620754f9162f8410160418325228b8f601
ARTA <rgaydm03@gmail.com>
2026-04-24 19:54:52 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'pixiedraw-v2026.04.24-shared-canonical-snapshot-open-fix1';
+const CACHE_VERSION = 'pixiedraw-v2026.04.24-shared-provisional-fix1';
 const CORE_ASSETS = [
   '/pixiedraw/',
   '/pixiedraw/index.html',
