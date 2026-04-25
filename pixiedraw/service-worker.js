ca4d8c8a2ac716f83f18780864c9744728b72677
ARTA <rgaydm03@gmail.com>
2026-04-25 12:41:53 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'pixiedraw-v2026.04.25-shared-replay-base-fix1';
+const CACHE_VERSION = 'pixiedraw-v2026.04.25-shared-realtime-stability-fix1';
 const CORE_ASSETS = [
   '/pixiedraw/',
   '/pixiedraw/index.html',
