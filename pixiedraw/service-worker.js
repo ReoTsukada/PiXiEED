2ea692c27ad3e5c1f196e833cf5c3f517ae5d2b4
ARTA <rgaydm03@gmail.com>
2026-05-30 19:02:16 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.05.29-supporter-serial-home';
+const APP_BUILD_VERSION = '2026.05.30-rail-min-ad';
 const CACHE_VERSION = `pixiedraw-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/pixiedraw/',
