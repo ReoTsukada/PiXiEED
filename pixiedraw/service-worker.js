8cf07d57493a64b85340b7d0d41f2ef4ba3d79b4
ARTA <rgaydm03@gmail.com>
2026-05-31 00:11:45 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.05.30-shared-create-progress';
+const APP_BUILD_VERSION = '2026.05.31-invite-copy-fallback';
 const CACHE_VERSION = `pixiedraw-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/pixiedraw/',
