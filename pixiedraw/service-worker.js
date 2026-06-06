f99eb5343f43cdfae7e9d21514c0aacb47b3ee28
ARTA <rgaydm03@gmail.com>
2026-06-07 03:56:07 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.06.07-canvas-resize-handle';
+const APP_BUILD_VERSION = '2026.06.07-stable-shared-ui';
 const CACHE_VERSION = `pixiedraw-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/pixiedraw/',
