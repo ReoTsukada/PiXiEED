b5df7f8bf61c0f64a2142b174987380041435fd1
ARTA <rgaydm03@gmail.com>
2026-07-03 21:25:37 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.06.12-soft-resume-resync';
+const APP_BUILD_VERSION = '2026.07.03-shared-auth-refresh-fix1';
 const CACHE_VERSION = `pixiedraw-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/pixiedraw/',
