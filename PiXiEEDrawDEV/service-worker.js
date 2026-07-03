44652dff023186e7d8f9dd35698cb0cc86e70f7c
ARTA <rgaydm03@gmail.com>
2026-07-03 18:51:57 +0900

    Update service-worker.js

--- PiXiEEDrawDEV/service-worker.js
+++ PiXiEEDrawDEV/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.07.03-pixieedrawdev-switch-v1';
+const APP_BUILD_VERSION = '2026.07.03-pixieedrawdev-switch-v2';
 const CACHE_VERSION = `pixieedrawdev-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/PiXiEEDrawDEV/',
