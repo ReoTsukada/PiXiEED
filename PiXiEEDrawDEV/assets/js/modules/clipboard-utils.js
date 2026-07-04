f621d0b2cfaebe922c0defb186edd9f5adf3073b
ARTA <rgaydm03@gmail.com>
2026-07-04 12:23:41 +0900

    Update clipboard-utils.js

--- PiXiEEDrawDEV/assets/js/modules/clipboard-utils.js
+++ PiXiEEDrawDEV/assets/js/modules/clipboard-utils.js
@@ -50,7 +50,7 @@
           localizeText('共有URLを手動でコピーしてください', 'Copy the shared URL manually'),
           value
         );
-        return true;
+        return false;
       } catch (error) {
         return false;
       }
