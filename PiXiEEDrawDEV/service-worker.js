0c688f79194efb6c49a0c69990690dfd1e39f85a
ARTA <rgaydm03@gmail.com>
2026-07-08 15:19:21 +0900

    Update service-worker.js

--- PiXiEEDrawDEV/service-worker.js
+++ PiXiEEDrawDEV/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.07.07-open-dialog-fix1';
+const APP_BUILD_VERSION = '2026.07.08-tabbar-leftlane-stable-fix1';
 const CACHE_VERSION = `pixieedrawdev-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/PiXiEEDrawDEV/',
