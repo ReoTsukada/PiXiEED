751933291d4a0d9e6822c01801172b17a549ee78
ARTA <rgaydm03@gmail.com>
2026-07-10 03:48:43 +0900

    Update service-worker.js

--- pixiedraw/service-worker.js
+++ pixiedraw/service-worker.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '2026.07.09-export-routing-fix2';
+const APP_BUILD_VERSION = '2026.07.10-pixiedraw-ads-restore1';
 const CACHE_VERSION = `pixiedraw-v${APP_BUILD_VERSION}`;
 const CORE_ASSETS = [
   '/pixiedraw/',
