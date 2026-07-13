0d33d9f6d5936e402a879c6738df9e0bb384ef80
ARTA <rgaydm03@gmail.com>
2026-07-13 09:26:31 +0900

    Update build-info.js

--- PiXiEEDrawDEV/assets/js/build-info.js
+++ PiXiEEDrawDEV/assets/js/build-info.js
@@ -3,7 +3,7 @@
   const buildInfo = Object.freeze({
     edition: 'dev',
     version: '0.9.0-dev.1',
-    buildId: '20260713-029',
+    buildId: '20260713-031',
     releasedAt: '2026-07-12T00:00:00+09:00',
   });
 
