0c8ce7872fa884339c18b0a2bdf253b833a2addb
ARTA <rgaydm03@gmail.com>
2026-07-15 14:07:13 +0900

    Update build-info.js

--- PiXiEEDrawDEV/assets/js/build-info.js
+++ PiXiEEDrawDEV/assets/js/build-info.js
@@ -3,13 +3,13 @@
   const buildInfo = Object.freeze({
     edition: 'dev',
     version: '0.9.0-dev.1',
-    buildId: '20260715-074',
-    releasedAt: '2026-07-14T00:00:00+09:00',
+    buildId: '20260715-075',
+    releasedAt: '2026-07-15T00:00:00+09:00',
   });
 
   runtime.__PIXIEEDRAW_BUILD_INFO__ = buildInfo;
   // Legacy consumers still use these aliases. The immutable object above is
   // the only source of the running build identity.
   runtime.__PIXIEEDRAW_BUILD_ID__ = buildInfo.buildId;
-  runtime.__PIXIEEDRAW_BUILD_REVISION__ = 20260714066;
+  runtime.__PIXIEEDRAW_BUILD_REVISION__ = 20260715075;
 })();
